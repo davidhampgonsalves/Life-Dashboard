7f76706bbf3aef66cb544b3cb3b11ea960366fdc
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2022-02-05 02:43:23 -0400

    parking ban title

--- serverless/lib/parkingBan.js
+++ serverless/lib/parkingBan.js
@@ -9,9 +9,9 @@ const cheerio = require('cheerio');
 module.exports.fetchMostRecentEvent = async () => {
   try {
     const $ = await rp({ uri: "https://www.halifax.ca/transportation/winter-operations/service-updates", headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36' }, transform: cheerio.load });
-    const title = $("#tablefield-paragraph-126041-field_table-0").find('.row_0.col_1.c-table__cell').text();
+    const status = $("#tablefield-paragraph-126041-field_table-0").find('.row_0.col_1.c-table__cell').text();
 
-    if(title.match(/ not /i))
+    if(status.match(/ not /i))
       return null;
 
     const start = moment.utc().startOf('day');
@@ -19,7 +19,7 @@ module.exports.fetchMostRecentEvent = async () => {
 
     return {
       icon: "🚗",
-      title,
+      title: "Parking ban in effect 1am-6am.",
 			start,
 			end,
 		};
