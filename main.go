b08fef565c03a57ec06c4586b552eccd103505c3
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2024-06-21 13:50:25 -0300

    support local hosting webserver

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+		"davidhampgonsalves/lifedashboard/pkg"
+		"io"
+		"bytes"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			png := pkg.Generate();
+			w.Header().Set("Content-Type", "image/png") // <-- set the content-type header
+			io.Copy(w, bytes.NewBuffer(png))
+    })
+
+    fmt.Println("Server listening on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
